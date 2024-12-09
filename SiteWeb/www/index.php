8a63325e8f7d2833fcb9ee8f411e72663bf3f1d7
jean-chadi <jean-chadi.frere@groupe-esigelec.org>
2024-12-09 14:54:38 +0100

    "Création des containers"

---
+++ SiteWeb/www/index.php
@@ -0,0 +1,16 @@
+<?php
+  session_start();
+  $titre = "Accueil";
+
+  include('header.inc.php');
+  include('menu.inc.php');
+
+  include('message.inc.php')
+?>
+
+  <h1>Début du projet</h1>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis saepe obcaecati quidem suscipit nam laudantium autem incidunt eum at voluptatum. Eligendi iusto modi neque similique cupiditate officia autem debitis ducimus omnis placeat. Voluptate repellat, ad temporibus quam repellendus in eos inventore, culpa nam atque beatae, mollitia vitae rem possimus quidem!</p>
+
+<?php
+  include('footer.inc.php');
+?>
