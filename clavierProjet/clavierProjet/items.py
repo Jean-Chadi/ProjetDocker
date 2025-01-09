cc5f82dd47f55fe82f0848f3e2a52ff943188e87
Thomas <thomas.paris@groupe-esigelec.org>
2025-01-09 14:59:07 +0100

    Modification de l'item clavier

--- clavierProjet/clavierProjet/items.py
+++ clavierProjet/clavierProjet/items.py
@@ -8,6 +8,9 @@ import scrapy
 
 class ClavierprojetItem(scrapy.Item):
     # define the fields for your item here like:
+    marque = scrapy.Field()
     titre = scrapy.Field()
+    image = scrapy.Field()
     prix = scrapy.Field()
+    lien = scrapy.Field()
     site = scrapy.Field()
